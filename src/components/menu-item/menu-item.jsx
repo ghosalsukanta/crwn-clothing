79b7300c41acbff4cdfdc6d6bfae87919e623f25
Sukanta Ghosal <sukanta.ghosal@outlook.com>
2021-04-26 00:26:18 +0530

    created separate component for hompage, styling mmenu items

--- src/components/menu-item/menu-item.jsx
+++ src/components/menu-item/menu-item.jsx
@@ -2,11 +2,16 @@ import React from 'react';
 import './menu-item.scss';
 
 
-const MenuItem = ({title}) =>
+const MenuItem = ({title,imageUrl,size}) =>
 (
-    <div className='menu-item'>
+    <div className={`${size} menu-item`}>
+
+        <div className='background-image' style={{
+        backgroundImage:`url(${imageUrl})`
+        }} />
+        
         <div className='content'>
-            <h1 className='title'>{title}</h1>
+            <h1 className='title'>{title.toUpperCase()}</h1>
             <span className='subtilte'>SHOP NOW</span>
         </div>
     </div>
